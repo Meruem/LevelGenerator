2190f088296d462022d585d24d61d1e8c9b0f37a
Martin Kuben <martin.kuben@seznam.cz>
2017-05-06 00:45:55 +0200

    db test

---
+++ LevelGenerator.Web/dbtest.fsx
@@ -0,0 +1,35 @@
+#I "../packages"
+#I "../build"
+#I "../../repository/packages"
+#I "../../repository/build"
+
+#r "WindowsAzure.Storage/lib/net45/Microsoft.WindowsAzure.Storage.dll"
+
+open System
+open System.IO
+//open Microsoft.Azure
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Table
+//open Microsoft.WindowsAzure.ServiceRuntime
+
+let connectionString = "DefaultEndpointsProtocol=https;AccountName=levgendb;AccountKey=Qvb3tTMJ9jXcbbr3lw9sxv2YHmqDw1DD5v0Ej/000bwoY/SnLpQ+YDEV0Xlj99EMrcjfzBy//qYUt5fRge1NcA==;EndpointSuffix=core.windows.net"
+let storageAccount = CloudStorageAccount.Parse connectionString
+let tableClient = storageAccount.CreateCloudTableClient ()
+let table = tableClient.GetTableReference("testTable")
+
+type Customer(firstName, lastName, email: string, phone: string) =
+    inherit TableEntity(partitionKey=lastName, rowKey=firstName)
+    new() = Customer(null, null, null, null)
+    member val Email = email with get, set
+    member val PhoneNumber = phone with get, set
+
+
+let customer = 
+    Customer("Walter", "Harp", "Walter@contoso.com", "425-555-0101")
+
+let insertOp = TableOperation.Insert(customer)
+let dostuff () = 
+    table.CreateIfNotExists () |> ignore
+    table.Execute(insertOp)
+
